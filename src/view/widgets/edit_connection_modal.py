f21543fcd0db7c8556a7d0dd113d1ba7479552fc
Jeremy <jqwez@icloud.com>
2024-01-14 20:44:59 -0500

    editable switches from entry to label #TODO connect to db

--- src/view/widgets/edit_connection_modal.py
+++ src/view/widgets/edit_connection_modal.py
@@ -15,17 +15,17 @@ class EditConnectionModal(tk.Toplevel):
     def populate_fields(self):
         self.action_label = ttk.Label(self, text="Edit Connection")
         self.action_label.grid(column=0, row=1)
-        self.name_label = ttk.Label(self, text=self.dao.name)
-        self.name_label.bind("<Button-1>", lambda event: self.handle_click_field(event))
-        self.name_label.grid(column=0, row=2)
-        self.cancel_button = ttk.Button(self, text="Cancel")
-        self.cancel_button.bind("<Button-1>", lambda event: self.handle_cancel())
-        self.cancel_button.grid(column=0, row=3)
-        self.submit_button = ttk.Button(self, text="Save")
-        self.submit_button.grid(column=0, row=4)
+        self.name_label = self.editable(text=self.dao.name, row=2) 
+        self.url_label = self.editable(text=self.dao.linkedin, row=3)
         self.columnconfigure(0, weight=1)
 
-    def handle_cancel(self):
+    def editable(self, text: str, row: int = 0) -> ttk.Label:
+        label: ttk.Label = ttk.Label(self, text=text)
+        label.bind("<Button-1>", self.handle_click_field)
+        label.grid(column=0, row=row)
+        return label
+        
+    def handle_cancel(self, event: tk.Event):
         self.destroy()
 
     def handle_click_field(self, event: tk.Event):
@@ -33,11 +33,23 @@ class EditConnectionModal(tk.Toplevel):
         content: str = widget.cget("text")
         grid_info: tk._GridInfo = widget.grid_info()
         widget.grid_forget()
-        entry = tk.Entry(self, textvariable=content)
+        widget.destroy()
+        entry = ttk.Entry(self, textvariable=content)
+        entry.bind("<Return>", self.handle_accept_entry)
+        entry.bind("<FocusOut>", self.handle_accept_entry)
         entry.delete(0, tk.END)
         entry.insert(0, content)
         entry.grid(column=grid_info["column"], row=grid_info["row"])
-        
+
+    def handle_accept_entry(self, event: tk.Event):
+        widget: tk.Widget = event.widget
+        content: str = widget.get()
+        grid_info: tk._GridInfo = widget.grid_info()
+        widget.grid_forget()
+        widget.destroy()
+        label = ttk.Label(self, text=content)
+        label.bind("<Button-1>", self.handle_cancel)
+        label.grid(column=grid_info["column"], row=grid_info["row"])
 
     def get_window_position(self) -> (int, int):
         if self.master:
